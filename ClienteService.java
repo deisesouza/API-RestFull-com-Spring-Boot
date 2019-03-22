9058b047323f1fbd7b52856e230a6637aa2a90c3
Deise Souza <deise_santana_souza@hotmail.com>
2019-03-22 06:36:50 -0300

    Update ClienteService.java

--- ClienteService.java
+++ ClienteService.java
@@ -26,9 +26,9 @@ public class ClienteService {
 
 	}
 	
-	//public Cliente buscarId(Integer id) {
-	//   return clienteRepository.findOne(id);
-	//}
+	public Integer buscarId(Integer id) {
+	   return clienteRepository.findOne(id);
+	}
 	
 	
 	public void excluir(Cliente cliente) {
